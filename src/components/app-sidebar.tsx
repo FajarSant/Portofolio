3e198d7f43fd28fd34481964f8cd70a7b118e881
Fajarsant <4jar100so@gmail.com>
2025-04-14 17:31:20 +0700

    `Refactor Home page to display a loading animation and redirect to /home after 3 seconds`

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -75,7 +75,7 @@ export function AppSidebar() {
           <SidebarGroupContent className="flex flex-col items-center py-8 px-4 mb-2 border-b-4">
             <div className="h-20 w-20 overflow-hidden rounded-full">
               <Image
-                src="/image/foto-saya.jpg"
+                src="/images/foto-saya.jpg"
                 alt="Profile Photo"
                 width={128}
                 height={128}
